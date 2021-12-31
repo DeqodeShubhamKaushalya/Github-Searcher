8cef7867d611a2dfba6ad9c11e6a82901e769716
DeqodeShubhamKaushalya <skaushalya@deqode.com>
2021-12-31 17:00:49 +0530

    feat:initial commit

--- cambium/src/routers/profile.router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import profileController from '../controllers/profile.controller';
-
-const router = express.Router();
-router.post('/:handle/profile', profileController.profile);
-
-export default router;
