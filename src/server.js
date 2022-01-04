8fcc2a9261d2b69182c08f19c329ba59549608a7
DeqodeShubhamKaushalya <skaushalya@deqode.com>
2022-01-04 12:43:25 +0530

    style: changed function name

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app';
 import config from './utils/config/config';
-import connectDb from './db/config/dbconnection';
+import connectDB from './db/config/dbconnection';
 import logger from './utils/logger';
 
 const port = config.get('port');
@@ -8,7 +8,7 @@ const port = config.get('port');
 // Server start
 async function appStart() {
   try {
-    await connectDb();
+    await connectDB();
     app.listen(port);
     logger.info(`app listening at port: ${port}`);
   } catch (err) {
